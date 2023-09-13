2cc5d9cc09d21f43364c0be8714b346c087162d9
Mika Sato <mika99@live.com.au>
2023-09-13 15:50:22 +1000

    set up basic file server

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
-const hostname = '127.0.0.1';
+const hostname = 'localhost';
 const port = 8080;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
+    console.log(`${req.method} ${req.url}`);
+
+    let filePath = req.url;
+    
+    if (filePath === '/') {
+        filePath = 'public/index.html';
+    } else {
+        filePath = 'public/' + req.url + '.html';
+    }
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('404: File not found');
+        } else {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
 });
 
 server.listen(port, hostname, () => {
